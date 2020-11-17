6dde54e21ff1f8fa3a64611e4f4a1dd0f4b7401e
Deimante <deimante.mileryte@gmail.com>
2020-11-17 14:36:54 +0100

    show date

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+import {getCurrentDate} from './utils';
 
 
 function App() {
     const [people, setPeople] = useState(data)
     return <main>
         <section className="container">
+       
+      <div className="date">Today is {getCurrentDate()}</div>
+
             <h3>{people.length} Birthdays today</h3>
             <List people={people}/>
             <button onClick={() => setPeople([])}>Clear All</button>
-
         </section>
     </main>
 }
