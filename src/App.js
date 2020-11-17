c54cf62d13693d950aa857a525dc6872b3b9b2f1
Deimante <deimante.mileryte@gmail.com>
2020-11-17 13:14:58 +0100

    showing people from the data.js file

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import List from './List';
 
 
 function App() {
+    const [people, setPeople] = useState(data)
     return <main>
         <section className="container">
-            <h3>0 Birthdays today</h3>
-            <List/>
+            <h3>{people.length} Birthdays today</h3>
+            <List people={people}/>
             <button onClick={() => console.log('You clicked me')}>Clear All</button>
 
         </section>
