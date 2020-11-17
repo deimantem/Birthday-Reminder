612c30b7d543f84e4d942936f2c69eb570305208
Deimante <deimante.mileryte@gmail.com>
2020-11-17 12:46:00 +0100

    base

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+
+function App() {
+    return <h2>Birthday Reminder Project setup</h2>
+}
+
+export default App;
